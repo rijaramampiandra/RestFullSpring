470d48159f0cb83860b0823f7f52050ab870e0c7
RRA3805 <RRA3805@INV010727.viseo.net>
2018-03-08 17:48:06 +0100

    <dev> Implement elastic search with spring restfull!

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 This is a spring rest for angular.
 
 Only http://localhost:8080 can hava access on this REST.
+
+To insert on elasticsearch :
+URL : http://localhost:9200/spring_rest/posts/1004
+Body :
+{
+  "title": "Spring + Spring Data + ElasticSearch",
+  "category":"Spring Boot",
+  "published_date":"23-MAR-2017",
+  "author":"Rambabu Posa"
+}
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,7 +1,18 @@
 package hello;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +21,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-    @CrossOrigin(origins = "http://localhost:4200")
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
+	@Value("${elasticsearch.index}")
+	private String esIndex;
+
+	@Value("${elasticsearch.index.id}")
+	private Integer esId;
+
+	@Value("${elasticsearch.host}")
+	private String esHost;
+
+	@Value("${elasticsearch.url}")
+	private String esUrl;
+
+	@Value("${elasticsearch.port}")
+	private Integer esPort;
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws IOException {
+
+		RestClient restClient = RestClient
+				.builder(new HttpHost(esUrl, esPort, esHost)).build();
+
+		searchData(restClient);
+
+		Map<String, String> params = Collections.emptyMap();
+		String jsonString = "{" + "\"title\":\"Spring + Spring Data + ElasticSearch\","
+				+ "\"category\":\"Spring Boot\"," + "\"published_date\":\"JAVA-08-MAR-2017\","
+				+ "\"author\":\"Rija RAMAMPIANDRA\"" + "}";
+		HttpEntity entity = new NStringEntity(jsonString, ContentType.APPLICATION_JSON);
+		Response response = restClient.performRequest("PUT", esIndex + "/" + esId , params, entity);
+		if (response.getEntity() != null) {
+			System.out.println("Entity not null");
+		}
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	private void searchData(RestClient restClient) throws IOException {
+		Map<String, String> params = Collections.emptyMap();
+		HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory consumerFactory = new HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory(
+				30 * 1024 * 1024);
+		Response response = restClient.performRequest("GET", esIndex + "/_search", params, null, consumerFactory);
+		if (response.getEntity() != null && response.getStatusLine() != null) {
+			System.out.println("Entity not null" + response.getStatusLine().getStatusCode());
+		}
+
+	}
 }
